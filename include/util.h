b2ef1481a06d4d53ba68ce6ae241d47840fd6539
Naphtha Nepanthez <naphtha@lotte.link>
2020-12-28 02:49:47 +0000

    align

--- include/util.h
+++ include/util.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <limits.h>
 
-#define __hh_rol(n, d) ((n << d) | (n >> ((sizeof(n) * CHAR_BIT) - d)))
-#define __hh_ror(n, d) ((n >> d) | (n << ((sizeof(n) * CHAR_BIT) - d)))
+#define __hh_rol(n, d)    ((n << d) | (n >> ((sizeof(n) * CHAR_BIT) - d)))
+#define __hh_ror(n, d)    ((n >> d) | (n << ((sizeof(n) * CHAR_BIT) - d)))
 #define __hh_swap_s(x, y) do { __typeof__(x) _SWAPVTMP = x; x = y; y = _SWAPVTMP; } while (0)
