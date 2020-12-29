79fdd1b147e6c387302dcc2ea3d3af58d1aff919
Naphtha Nepanthez <naphtha@lotte.link>
2020-12-29 07:12:37 +0000

    ass safety

--- include/svec.h
+++ include/svec.h
@@ -6,8 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* WARNING: ACTUAL NAPHTHAMANCY BELOW. AVOID HAVING YOUR SOUL CONSUMED BY THE VOID. */
-
 #ifndef HH_DYN_NO_SHORTHAND          /* TIP: Define HH_DYN_NO_SHORTHAND to disable the simplified interface.         */
 #define da_make    __hh_dyn_mk       /* Create a dynamic array of the specified type                                 */
 #define da_free    __hh_dyn_free     /* Free a dynamic array, deallocating all of its memory includng the metadata   */
@@ -24,6 +22,8 @@
 #define da_lastptr __hh_dyn_last_ptr /* Get void pointer to the last element in the dynamic array, NULL if empty     */
 #endif
 
+/* ----------- ---------------- IF POSSIBLE, PLEASE USE THE SIMPLIFIED INTERFACE ABOVE! ---------------- ----------- */
+
 #define HH_DYN_BASIS NULL
 #define __hh_dyn(name, type)   type *name = HH_DYN_BASIS
 #define __hh_dyn_init(a)       (__hh_dyn_add((a), 0)) /* Must be type-aware */
@@ -34,24 +34,27 @@
 #define __hh_dyn_set_els(a, n) (hh_i_dyn_set_els((void **)&(a), (n), __hh_i_dyn_sas((a))))
 #define __hh_dyn_add(a, n)     (__hh_dyn_set_els((a), __hh_dyn_count((a)) + (n)))
 #define __hh_dyn_push(a, v)    ({hh_stat_t __99tmp = __hh_dyn_add((a), 1); if (__99tmp == HH_STATUS_OKAY)\
-                               ((__typeof__((v)) *) a)[__hh_dyn_last_idx((a))] = (v); __99tmp;})
-#define __hh_dyn_last(a, d)    (__hh_dyn_count((a)) > 0 ? ((__typeof__((d)) *) a)[__hh_dyn_last_idx((a))] : (d))
+                               (__hh_i_dyn_ecl((a), __typeof__((v)))) = (v); __99tmp;})
+#define __hh_dyn_last(a, d)    (__hh_dyn_count((a)) > 0 ? __hh_i_dyn_ecl((a), __typeof__((d))) : (d))
 #define __hh_dyn_mk(type)      ({__hh_dyn(__98tmp, type); hh_stat_t __97tmp = __hh_dyn_init(__98tmp);\
                                (__97tmp != HH_STATUS_OKAY ? NULL : __98tmp);})
 #define __hh_dyn_ins(a, i, v)  ({__hh_dyn_init((a)); __typeof__(v) __96tmp = (v);\
                                (hh_i_dyn_ins((void **)&(a), (i), &__96tmp));})
 #define __hh_dyn_del(a, i)     ({__hh_dyn_init((a)); hh_i_dyn_del((void **)&(a), (i));})
 #define __hh_dyn_shrkby(a, n)  (__hh_dyn_add((a), -(n)))
-#define __hh_dyn_last_ptr(a)   ((__hh_dyn_count((a)) > 0) ? ((void *)(&(((char (*)[__hh_i_dyn_s(__hh_i_dyn_raw(a))])a)\
-                               [__hh_dyn_last_idx(a)]))) : NULL)
+#define __hh_dyn_last_ptr(a)   ((__hh_dyn_count((a)) > 0) ? (__hh_i_dyn_ecg((a), __hh_dyn_last_idx((a)))) : NULL)
 
 /* ---------------- ---------------- !!! EVERYTHING BELOW THIS LINE IS PRIVATE !!! ---------------- ---------------- */
 
-#define __hh_i_dyn_raw(a)    ((a) ? ((size_t *) (void *) (a)) - 2 : NULL)           /* RETURN: r (raw array)         */
-#define __hh_i_dyn_c(r)      ((r) ? (((size_t *)(r))[0]) : 0)                       /* RETURN: c (element count)     */
-#define __hh_i_dyn_s(r)      ((r) ? (((size_t *)(r))[1]) : 0)                       /* RETURN: s (element size)      */
-#define __hh_i_dyn_trs(c, s) ((sizeof(size_t) * 2) + ((c) * (s)))                   /* RETURN: t (total raw size)    */
-#define __hh_i_dyn_sas(a)    ((a) ? __hh_i_dyn_s(__hh_i_dyn_raw(a)) : sizeof(*(a))) /* RETURN: s (TA! element size)  */
+#define __hh_i_dyn_raw(a)       ((a) ? ((size_t *) (void *) (a)) - 2 : NULL)           /* RET: r (raw array)         */
+#define __hh_i_dyn_c(r)         ((r) ? (((size_t *)(r))[0]) : 0)                       /* RET: c (element count)     */
+#define __hh_i_dyn_s(r)         ((r) ? (((size_t *)(r))[1]) : 0)                       /* RET: s (element size)      */
+#define __hh_i_dyn_trs(c, s)    ((sizeof(size_t) * 2) + ((c) * (s)))                   /* RET: t (total raw size)    */
+#define __hh_i_dyn_sas(a)       ((a) ? __hh_i_dyn_s(__hh_i_dyn_raw(a)) : sizeof(*(a))) /* RET: s (TA! element size)  */
+#define __hh_i_dyn_eci(a)       ((char (*)[__hh_i_dyn_s(__hh_i_dyn_raw((a)))])(a))
+#define __hh_i_dyn_ecg(a, i)    ((void *)(&(__hh_i_dyn_eci((a))[(i)])))
+#define __hh_i_dyn_ect(a, i, t) (((t *)(__hh_i_dyn_ecg((a), (i))))[0])
+#define __hh_i_dyn_ecl(a, t)    (__hh_i_dyn_ect((a), __hh_dyn_last_idx((a)), t))
 
 HH_EXTERN hh_status_t hh_i_dyn_set_els(void **a, size_t n, size_t e);
 HH_EXTERN hh_status_t hh_i_dyn_ins(void **a, size_t i, void *e);
