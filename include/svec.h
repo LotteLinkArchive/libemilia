001230540587eac137e3a39986db707b5567dba2
Naphtha Nepanthez <naphtha@lotte.link>
2020-12-30 11:11:07 +0000

    hells

--- include/svec.h
+++ include/svec.h
@@ -20,8 +20,7 @@
 #define da_insert  __hh_dyn_ins      /* Insert an element into the dynamic array at the given index.                 */
 #define da_setsize __hh_dyn_set_els  /* Set the amount of elements in the dynamic array.                             */
 #define da_lastptr __hh_dyn_last_ptr /* Get void pointer to the last element in the dynamic array, NULL if empty     */
-#endif                               /* WARN: da_make, da_last and da_push must retain the same type for the array. 
-                                        WARN: da_insert requires `v` to be the same width as `a`'s non-ptr type.     */
+#endif                               /* WARN: da_make, da_insert, da_last and da_push must retain the same a-type.   */
 
 /* ----------- ---------------- IF POSSIBLE, PLEASE USE THE SIMPLIFIED INTERFACE ABOVE! ---------------- ----------- */
 
@@ -39,12 +38,15 @@
 #define __hh_dyn_last(a, d)    (__hh_dyn_count((a)) > 0 ? (a)[__hh_dyn_last_idx((a))] : (d))
 #define __hh_dyn_mk(type)      ({__hh_dyn(__98tmp, type); hh_stat_t __97tmp = __hh_dyn_init(__98tmp);\
                                (__97tmp != HH_STATUS_OKAY ? NULL : __98tmp);})
-#define __hh_dyn_ins(a, i, v)  ({__hh_dyn_init((a)); __typeof__(v) __96tmp = (v);\
+#define __hh_dyn_ins(a, i, v)  ({__hh_dyn_init((a)); __typeof__((a)[0]) __96tmp = (v);\
                                (hh_i_dyn_ins((void **)&(a), (i), &__96tmp));})
 #define __hh_dyn_del(a, i)     ({__hh_dyn_init((a)); hh_i_dyn_del((void **)&(a), (i));})
 #define __hh_dyn_shrkby(a, n)  (__hh_dyn_add((a), -(n)))
 #define __hh_dyn_last_ptr(a)   ((__hh_dyn_count((a)) > 0) ? (__hh_i_dyn_ecg((a), __hh_dyn_last_idx((a)))) : NULL)
 
+/* These are some highly experimental macros that allow you to push and get elements from the dynamic array after
+   casting it to a different type. I wouldn't recommend using these in production, and they completely break the casts
+   of individual elements, but if you desparately need this, it's here, and you can use it as your own risk. Enjoy.  */
 #define __hh_dyn_pushtt(a, v)  ({hh_stat_t __99tmp = __hh_dyn_add((a), 1); if (__99tmp == HH_STATUS_OKAY)\
                                (__hh_i_dyn_ecl((a), __typeof__((v)))) = (v); __99tmp;})
 #define __hh_dyn_lasttt(a, d)  (__hh_dyn_count((a)) > 0 ? __hh_i_dyn_ecl((a), __typeof__((d))) : (d))
