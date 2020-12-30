70a0a0af520ebad76cacf12b36c5256db1364ec9
Naphtha Nepanthez <naphtha@lotte.link>
2020-12-30 10:58:27 +0000

    Size bugfix

--- include/svec.h
+++ include/svec.h
@@ -34,8 +34,8 @@
 #define __hh_dyn_set_els(a, n) (hh_i_dyn_set_els((void **)&(a), (n), __hh_i_dyn_sas((a))))
 #define __hh_dyn_add(a, n)     (__hh_dyn_set_els((a), __hh_dyn_count((a)) + (n)))
 #define __hh_dyn_push(a, v)    ({hh_stat_t __99tmp = __hh_dyn_add((a), 1); if (__99tmp == HH_STATUS_OKAY)\
-                               (__hh_i_dyn_ecl((a), __typeof__((v)))) = (v); __99tmp;})
-#define __hh_dyn_last(a, d)    (__hh_dyn_count((a)) > 0 ? __hh_i_dyn_ecl((a), __typeof__((d))) : (d))
+                               (a)[__hh_dyn_last_idx((a))] = (v); __99tmp;})
+#define __hh_dyn_last(a, d)    (__hh_dyn_count((a)) > 0 ? (a)[__hh_dyn_last_idx((a))] : (d))
 #define __hh_dyn_mk(type)      ({__hh_dyn(__98tmp, type); hh_stat_t __97tmp = __hh_dyn_init(__98tmp);\
                                (__97tmp != HH_STATUS_OKAY ? NULL : __98tmp);})
 #define __hh_dyn_ins(a, i, v)  ({__hh_dyn_init((a)); __typeof__(v) __96tmp = (v);\
@@ -43,6 +43,10 @@
 #define __hh_dyn_del(a, i)     ({__hh_dyn_init((a)); hh_i_dyn_del((void **)&(a), (i));})
 #define __hh_dyn_shrkby(a, n)  (__hh_dyn_add((a), -(n)))
 #define __hh_dyn_last_ptr(a)   ((__hh_dyn_count((a)) > 0) ? (__hh_i_dyn_ecg((a), __hh_dyn_last_idx((a)))) : NULL)
+
+#define __hh_dyn_pushtt(a, v)  ({hh_stat_t __99tmp = __hh_dyn_add((a), 1); if (__99tmp == HH_STATUS_OKAY)\
+                               (__hh_i_dyn_ecl((a), __typeof__((v)))) = (v); __99tmp;})
+#define __hh_dyn_lasttt(a, d)  (__hh_dyn_count((a)) > 0 ? __hh_i_dyn_ecl((a), __typeof__((d))) : (d))
 
 /* ---------------- ---------------- !!! EVERYTHING BELOW THIS LINE IS PRIVATE !!! ---------------- ---------------- */
 
