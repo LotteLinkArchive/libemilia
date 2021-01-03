5d44d55da99e6c57812b36f353bac735fe55273f
Naphtha Nepanthez <naphtha@lotte.link>
2021-01-03 18:30:50 +0000

    Entropy generator improvements

--- src/entropygen.c
+++ src/entropygen.c
@@ -12,8 +12,12 @@ static uint8_t hh_entropy_p8()
    clock_t t = clock();
    
    do {
-      v ^= reduce8(rand());
-      v ^= reduce8(XXH32(&v, sizeof(v), 0));
+      v ^= reduce8(rand()) ^ reduce8(t);
+      union {
+         uint32_t hash;
+         uint8_t parts[4];
+      } hash = {.hash = XXH32(&v, sizeof(v), 0)};
+      v ^= hash.parts[0] ^ hash.parts[1] ^ hash.parts[2] ^ hash.parts[3];
    } while ((clock() - t) < (CLOCKS_PER_SEC >> 8));
    
    return v;
@@ -28,7 +32,7 @@ uint64_t hh_entropy_seed64()
    } pri;
    
    for (unsigned int x = 0; x < 8; x++) {
-      pri.s[x] = hh_entropy_p8();
+      pri.s[x] ^= hh_entropy_p8();
    }
    
    return pri.f;
