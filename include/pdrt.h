8c5cf54276b6ff2ead471288e860db43e61f0e54
Naphtha Nepanthez <naphtha@lotte.link>
2021-02-19 04:47:21 +0000

    tyler1 screaming HELP

--- include/pdrt.h
+++ include/pdrt.h
@@ -14,28 +14,39 @@
 enum em_pdrt_types_e {
    /* Internal primitives - In many cases, these can be collapsed into Bool,
       Integer, Unsigned Integer and Real */
-   EM_DTTYPE_BOOL,
-   EM_DTTYPE_SI8,
-   EM_DTTYPE_UI8,
-   EM_DTTYPE_SI16,
-   EM_DTTYPE_UI16,
-   EM_DTTYPE_SI32,
-   EM_DTTYPE_UI32,
-   EM_DTTYPE_SI64,
-   EM_DTTYPE_UI64,
-   EM_DTTYPE_R64,
+   EM_DTTYPE_BOOL = 1,
+   EM_DTTYPE_SI8 = 2,
+   EM_DTTYPE_UI8 = 4,
+   EM_DTTYPE_SI16 = 8,
+   EM_DTTYPE_UI16 = 16,
+   EM_DTTYPE_SI32 = 32,
+   EM_DTTYPE_UI32 = 64,
+   EM_DTTYPE_SI64 = 128,
+   EM_DTTYPE_UI64 = 256,
+   EM_DTTYPE_R64 = 512,
 
    /* Lists store different types (inefficient), arrays store constant type
       (efficient) */
-   EM_DTTYPE_LIST,
-   EM_DTTYPE_ARR,
-   EM_DTTYPE_STR,
+   EM_DTTYPE_LIST = 1024,
+   EM_DTTYPE_ARR = 2048,
+   EM_DTTYPE_STR = 4096,
 
    /* Complex types */
-   EM_DTTYPE_DICT
+   EM_DTTYPE_DICT = 8192,
+
+   /* Invalid type */
+   EM_DTTYPE_FAIL = 16384
 };
 
-typedef char em_pdrt_type_t;
+#define EM_PDRT_PRIMS 0x03FF
+#define EM_PDRT_ITERS 0x1C00
+#define EM_PDRT_CMPLX 0x2000
+#define EM_PDRT_NPRIM 0x7C00
+#define EM_PDRT_ODPRI 0x0201
+#define EM_PDRT_ALLSI 0x00AA
+#define EM_PDRT_ALLUI 0x0154
+
+typedef unsigned short em_pdrt_type_t;
 
 union em_pdrt_buft_u {
    void *ptr;
@@ -63,3 +74,4 @@ typedef struct em_pdrt_el_s em_pdrt_el_t;
 #define em_pdrt_getrl(el) ((el).buf.real)
 #define em_pdrt_getbl(el) ((el).buf.boolt)
 #define em_pdrt_getbf(el) ((el).buf.ptr)
+#define em_pdrt_getty(el) ((el).type)
--- src/pdrt.c
+++ src/pdrt.c
@@ -93,6 +93,33 @@ em_pdrt_el_t *em_pdrt_getle(em_pdrt_el_t *list, uint64_t i)
    return &((em_pdrt_el_t *)list->buf.ptr)[i];
 }
 
+em_pdrt_el_t em_pdrt_getae(em_pdrt_el_t *iterable, uint64_t i)
+{
+   if (iterable->type == EM_DTTYPE_LIST) return *em_pdrt_getle(iterable, i);
+
+   em_pdrt_el_t tel = { .type = iterable->atype };
+
+   switch (tel.type) {
+#define TMC(it, bt, rt)\
+   case it:\
+      tel.buf.bt = ((rt *)iterable->buf.ptr)[i];\
+      break;
+   TMC(EM_DTTYPE_BOOL, boolt, bool);
+   TMC(EM_DTTYPE_SI8, sint, int8_t);
+   TMC(EM_DTTYPE_SI16, sint, int16_t);
+   TMC(EM_DTTYPE_SI32, sint, int32_t);
+   TMC(EM_DTTYPE_SI64, sint, int64_t);
+   TMC(EM_DTTYPE_UI8, uint, uint8_t);
+   TMC(EM_DTTYPE_UI16, uint, uint16_t);
+   TMC(EM_DTTYPE_UI32, uint, uint32_t);
+   TMC(EM_DTTYPE_UI64, uint, uint64_t);
+   TMC(EM_DTTYPE_R64, real, double);
+#undef TMC
+   }
+
+   return tel;
+}
+
 em_status_t em_pdrt_mkdict(em_pdrt_el_t *target)
 {
    em_pdrt_el_t y = em_pdrt_mkelist();
@@ -115,7 +142,7 @@ uint64_t em_pdrt_getlen(em_pdrt_el_t *x)
    if (x->type == EM_DTTYPE_STR)
       return x->len - 1;
    else if (x->type == EM_DTTYPE_DICT)
-      return em_pdrt_getle(x, 0)->len;
+      return em_pdrt_getae(x, 0).len;
    else
       return x->len;
 }
@@ -124,8 +151,8 @@ bool em_pdrt_cmpel(em_pdrt_el_t *x, em_pdrt_el_t *y)
 {
    if (x->type != y->type)
       return false;
-   if (x->type == EM_DTTYPE_STR)
-      return memcmp(x->buf.ptr, y->buf.ptr, x->len - 1);
+   if (x->type == EM_DTTYPE_STR || x->type == EM_DTTYPE_ARR)
+      return memcmp(x->buf.ptr, y->buf.ptr, em_pdrt_getlen(x));
    if (x->type == EM_DTTYPE_LIST)
       return memcmp(x->buf.ptr, y->buf.ptr, x->len * sizeof(em_pdrt_el_t));
    if (x->type == EM_DTTYPE_DICT)
@@ -140,9 +167,11 @@ bool em_pdrt_cmpel(em_pdrt_el_t *x, em_pdrt_el_t *y)
 
 int64_t em_pdrt_dsearch(em_pdrt_el_t *dict, em_pdrt_el_t key)
 {
-   for (uint64_t i = 0; i < em_pdrt_getlen(dict); i++)
-      if (em_pdrt_cmpel(&key, em_pdrt_getle(em_pdrt_getle(dict, 0), i)))
+   for (uint64_t i = 0; i < em_pdrt_getlen(dict); i++) {
+      em_pdrt_el_t cbf = em_pdrt_getae(em_pdrt_getle(dict, 0), i);
+      if (em_pdrt_cmpel(&key, &cbf))
          return i;
+   }
 
    return -1;
 }
@@ -164,6 +193,7 @@ em_status_t em_pdrt_dadd(em_pdrt_el_t *dict, em_pdrt_el_t key,
 
 em_pdrt_el_t *em_pdrt_dget(em_pdrt_el_t *dict, em_pdrt_el_t key)
 {
+   /* TODO: Handle compressed dict */
    int64_t i = em_pdrt_dsearch(dict, key);
    if (i < 0)
       return NULL;
@@ -181,10 +211,117 @@ void em_pdrt_free(em_pdrt_el_t *el)
       goto arec_del;
    }
 
-   if (el->buf.ptr && (el->type == EM_DTTYPE_STR)) {
+   if (el->buf.ptr &&
+       (el->type == EM_DTTYPE_STR || el->type == EM_DTTYPE_ARR)) {
    arec_del:
       free(el->buf.ptr);
       el->buf.ptr = NULL;
       el->len = 0;
    }
+}
+
+em_pdrt_type_t em_pdrt_calcrn(em_pdrt_el_t *list)
+{
+   if (list->type != EM_DTTYPE_LIST) return EM_DTTYPE_FAIL;
+
+   em_pdrt_type_t typezone = 0;
+   for (uint64_t x = 0; x < list->len; x++)
+      typezone |= em_pdrt_getle(list, x)->type;
+
+   if (__builtin_popcount(typezone) == 1 && typezone & EM_PDRT_PRIMS)
+      return typezone;
+
+   if (typezone & EM_PDRT_PRIMS &&
+       !(typezone & (EM_PDRT_NPRIM | EM_PDRT_ODPRI))) {
+#define TZC(st, ut, fs)                                                        \
+   do {                                                                        \
+      if (typezone & ((st) | (ut)))                                            \
+         return (fs);                                                          \
+      if (typezone & (ut))                                                     \
+         return (ut);                                                          \
+      if (typezone & (st))                                                     \
+         return (st);                                                          \
+   } while (0)
+
+      TZC(EM_DTTYPE_SI64, EM_DTTYPE_UI64, EM_DTTYPE_FAIL);
+      TZC(EM_DTTYPE_SI32, EM_DTTYPE_UI32, EM_DTTYPE_SI64);
+      TZC(EM_DTTYPE_SI16, EM_DTTYPE_UI16, EM_DTTYPE_SI32);
+      TZC(EM_DTTYPE_SI8, EM_DTTYPE_UI8, EM_DTTYPE_SI16);
+
+#undef TZC
+   }
+
+   return EM_DTTYPE_FAIL;
+}
+
+unsigned char em_pdrt_twidth(em_pdrt_type_t x)
+{
+   switch (x) {
+   case EM_DTTYPE_BOOL:
+      return sizeof(bool);
+   case EM_DTTYPE_SI8:
+   case EM_DTTYPE_UI8:
+      return 1;
+   case EM_DTTYPE_SI16:
+   case EM_DTTYPE_UI16:
+      return 2;
+   case EM_DTTYPE_SI32:
+   case EM_DTTYPE_UI32:
+      return 4;
+   case EM_DTTYPE_SI64:
+   case EM_DTTYPE_UI64:
+   case EM_DTTYPE_R64:
+      return 8;
+   default:
+      return 0;
+   }
+}
+
+em_status_t em_pdrt_convarr(em_pdrt_el_t *list)
+{
+   if (list->type != EM_DTTYPE_LIST)
+      return EM_STATUS_OKAY;
+
+   em_pdrt_type_t gtype = em_pdrt_calcrn(list);
+   if (gtype == EM_DTTYPE_FAIL)
+      return EM_STATUS_OKAY;
+
+   unsigned char twidth = em_pdrt_twidth(gtype);
+   void *nbuf = malloc(list->len * twidth);
+   if (!nbuf)
+      return EM_OUT_OF_MEMORY; /* SAFE */
+
+   list->type = EM_DTTYPE_ARR;
+   list->atype = gtype;
+
+   for (uint64_t x = 0; x < list->len; x++)
+      switch (gtype) {
+      case EM_DTTYPE_BOOL:
+         ((bool *)nbuf)[x] = em_pdrt_getle(list, x)->buf.boolt;
+         break;
+      case EM_DTTYPE_R64:
+         ((double *)nbuf)[x] = em_pdrt_getle(list, x)->buf.real;
+         break;
+      case EM_DTTYPE_UI8:
+      case EM_DTTYPE_UI16:
+      case EM_DTTYPE_UI32:
+      case EM_DTTYPE_UI64:
+         memcpy((char *)nbuf + (twidth * x),
+                &(em_pdrt_getle(list, x)->buf.uint), twidth);
+         break;
+#define SCONVS(dt, rt)                                                         \
+   case dt:                                                                    \
+      ((rt *)nbuf)[x] = em_pdrt_getle(list, x)->buf.sint;                      \
+      break;
+         SCONVS(EM_DTTYPE_SI8, int8_t);
+         SCONVS(EM_DTTYPE_SI16, int16_t);
+         SCONVS(EM_DTTYPE_SI32, int32_t);
+         SCONVS(EM_DTTYPE_SI64, int64_t);
+#undef SCONVS
+      }
+
+   free(list->buf.ptr);
+   list->buf.ptr = nbuf;
+
+   return EM_STATUS_OKAY;
 }
